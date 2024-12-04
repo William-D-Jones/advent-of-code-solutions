ba0e2b1bfe7a9f9c3ab01e33db2b5009e94bf435
William Jones <joneswilliamd@hotmail.com>
2024-12-04 18:25:32 -0500

    improve regular expressions for 2024 day 3

--- day3.py
+++ day3.py
@@ -7,31 +7,28 @@ import re
 # parsing
 X = open(sys.argv[1], 'r').read().strip()
 
-# part 1
-ans = 0
-re_mul = re.compile("^mul\(([0-9]{1,3}),([0-9]{1,3})\)")
-for i in range(len(X)):
-    match_mul = re_mul.search(X[i:])
-    if match_mul is not None:
-        ans += int(match_mul[1]) * int(match_mul[2])
-print(ans)
-
-# part 2
-ans = 0
+ans1 = 0
+ans2 = 0
 re_mul = re.compile("^mul\(([0-9]{1,3}),([0-9]{1,3})\)")
 re_do = re.compile("^do\(\)")
 re_dont = re.compile("^don't\(\)")
 ins = True
-for i in range(len(X)):
-    match_do = re_do.search(X[i:])
-    match_dont = re_dont.search(X[i:])
+i = 0
+len_max = len("mul(000,000)")
+while i < len(X):
+    match_do = re_do.search(X[i:i+len_max])
+    match_dont = re_dont.search(X[i:i+len_max])
     if match_do is not None:
         ins = True
+        i += len("do()") - 1
     elif match_dont is not None:
         ins = False
-    elif ins:
-        match_mul = re_mul.search(X[i:])
-        if match_mul is not None:
-            ans += int(match_mul[1]) * int(match_mul[2])
-print(ans)
-
+        i += len("don't()") - 1
+    match_mul = re_mul.search(X[i:])
+    if match_mul is not None:
+        ans1 += int(match_mul[1]) * int(match_mul[2])
+        ans2 += (int(match_mul[1]) * int(match_mul[2]) if ins else 0)
+        i += len(match_mul[0]) - 1
+    i += 1
+print(ans1)
+print(ans2)
