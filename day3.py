7ae59dc86fb73831f496a532d1998a971789fa0e
William Jones <joneswilliamd@hotmail.com>
2024-12-04 17:58:21 -0500

    use regular expressions for 2024 day 3

--- day3.py
+++ day3.py
@@ -5,76 +5,33 @@ from collections import defaultdict
 import re
 
 # parsing
-X = [ l.strip() for l in open(sys.argv[1], 'r') ]
+X = open(sys.argv[1], 'r').read().strip()
 
 # part 1
-Ins = []
-is_ins = False
-ins = []
-num = ''
-for x in X:
-    i = 0
-    while i < len(x):
-        c = x[i:i+1]
-        c4 = x[i:i+4]
-        if c == ',' and is_ins and len(ins) == 0 and num != '':
-            ins.append(int(num))
-            num = ''
-        elif c == ')' and is_ins and len(ins) == 1:
-            is_ins = False
-            ins.append(int(num))
-            Ins.append(ins)
-        elif c4 == 'mul(':
-            ins = []
-            is_ins = True
-            num = ''
-            i += 3
-        elif c in '0123456789' and is_ins:
-            num = ''.join([num, c])
-        else:
-            is_ins = False
-        i += 1
 ans = 0
-for ins in Ins:
-    ans += ins[0] * ins[1]
+re_mul = re.compile("^mul\(([0-9]{1,3}),([0-9]{1,3})\)")
+for i in range(len(X)):
+    match_mul = re_mul.search(X[i:])
+    if match_mul is not None:
+        ans += int(match_mul[1]) * int(match_mul[2])
 print(ans)
 
 # part 2
-Ins = []
-is_ins = False
-ins = []
-num = ''
-en = True
-for x in X:
-    i = 0
-    while i < len(x):
-        c = x[i:i+1]
-        c4 = x[i:i+4]
-        if x[i:i+7] == "don't()":
-            en = False
-            is_ins = False
-            i += 6
-        elif x[i:i+4] == "do()":
-            en = True
-            i += 3
-        elif c == ',' and is_ins and len(ins) == 0 and num != '':
-            ins.append(int(num))
-            num = ''
-        elif c == ')' and is_ins and len(ins) == 1:
-            is_ins = False
-            ins.append(int(num))
-            Ins.append(ins)
-        elif c4 == 'mul(' and en:
-            ins = []
-            is_ins = True
-            num = ''
-            i += 3
-        elif c in '0123456789' and is_ins:
-            num = ''.join([num, c])
-        else:
-            is_ins = False
-        i += 1
 ans = 0
-for ins in Ins:
-    ans += ins[0] * ins[1]
+re_mul = re.compile("^mul\(([0-9]{1,3}),([0-9]{1,3})\)")
+re_do = re.compile("^do\(\)")
+re_dont = re.compile("^don't\(\)")
+ins = True
+for i in range(len(X)):
+    match_do = re_do.search(X[i:])
+    match_dont = re_dont.search(X[i:])
+    if match_do is not None:
+        ins = True
+    elif match_dont is not None:
+        ins = False
+    elif ins:
+        match_mul = re_mul.search(X[i:])
+        if match_mul is not None:
+            ans += int(match_mul[1]) * int(match_mul[2])
 print(ans)
+
