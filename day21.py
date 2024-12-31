ed5da2fb0b2aafe2b232c61593b764be20bd2b96
William Jones <joneswilliamd@hotmail.com>
2024-12-31 10:20:37 -0500

    completed 2024 day 21 part 1

--- day21.py
+++ day21.py
@@ -1,102 +1,101 @@
 import sys
 
+# the numeric keypad
 KNUM = [['7', '8', '9'], ['4', '5', '6'], ['1', '2', '3'], ['', '0', 'A']]
+# the directional keypad
 KDIR = [['', '^', 'A'], ['<', 'v', '>']]
+# dictionary to convert directional keypad presses to directions
 OPS = {'^':(-1,0), 'v':(1,0), '<':(0,-1), '>':(0,1), 'A':'A'}
-EFF = {}
-for r in range(len(KDIR)):
-    for c in range(len(KDIR[r])):
-        press = KDIR[r][c]
-        if press != '':
-            EFF[press] = (r,c)
-print(EFF)
+# ictionary to convert directions to directional keypad presses
+iOPS = {value:key for key,value in OPS.items()}
 
-def get_paths(start, pad, target):
-    nrow = len(pad)
-    ncol = len(pad[0])
-    Keys = [''] # what keys we have pressed
-    Paths = [[start]] # what coordinates we have taken
-    Outs = [''] # what outputs we have produced
-    Seen = [set([start])] # what coordinates we have seen since the last A
-    Eff = [0] # the efficiency to get to the next button press
-    shortest = None
-    typed = '' # the best output we have so far
-    while not all(out == target for out in Outs) \
-    and (shortest is None or all(len(path) == shortest for path in Paths)):
-        Keys_Next = []
-        Paths_Next = []
-        Outs_Next = []
-        Seen_Next = []
-        Eff_Next = []
-        for key,path,out,seen,eff in zip(Keys,Paths,Outs,Seen,Eff):
-            # check if we have typed the target string
-            if out == target:
-                if len(path) == shortest:
-                    Keys_Next.append(key)
-                    Paths_Next.append(path)
-                    Outs_Next.append(out)
-                    Seen_Next.append(seen)
-                continue
-            # check if we can add another operation to the current path
-            elif shortest is not None and len(path) + 1 > shortest:
-                continue
-            # check all available operations
-            for press,op in OPS.items():
-                if out != typed and op != 'A':
-                    # do not translate the arm if we must type to keep up
-                    continue
-                if op != 'A': # the key press translates the arm
-                    # compute the next coordinate
-                    dr,dc = op
-                    coord = (path[-1][0]+dr, path[-1][1]+dc)
-                    # check if the coordinate is valid
-                    if 0 <= coord[0] < nrow and 0 <= coord[1] < ncol \
-                    and pad[coord[0]][coord[1]] != '' \
-                    and (shortest is None or shortest > len(path)+1) \
-                    and coord not in seen:
-                        Keys_Next.append(key + press)
-                        Paths_Next.append(path + [coord])
-                        Outs_Next.append(out)
-                        Seen_Next.append(seen | set([coord]))
-                else: # the key press extends the arm
-                    # add a character to the output
-                    out += pad[ path[-1][0] ][ path[-1][1] ]
-                    # check if the output is valid
-                    if target.startswith(out):
-                        Keys_Next.append(key + press)
-                        Paths_Next.append(path + [path[-1]])
-                        Outs_Next.append(out)
-                        Seen_Next.append(set([path[-1]]))
-                        # check if the target has been found
-                        if target == out:
-                            if shortest is None or shortest > len(path)+1:
-                                shortest = len(path)+1
-                        # if target not found, remember what is typed so far
-                        else:
-                            typed = out
-        Keys = Keys_Next
-        Paths = Paths_Next
-        Outs = Outs_Next
-        Seen = Seen_Next
+def get_key_pair(pad, start, end):
+    """
+    Get the key-presses required to journey on PAD from the START character
+    to the END character.
+    """
+    if start == end:
+        return 'A'
+    for r,row in enumerate(pad):
+        for c,char in enumerate(row):
+            if char == start:
+                coord_start = (r,c)
+            if char == end:
+                coord_end = (r,c)
+            if char == '':
+                coord_blank = (r,c)
+    # there are two shortest paths between points
+    # one path may need to be dropped if it contains a blank square
+    # compute Manhattan distances
+    dr = coord_end[0] - coord_start[0]
+    dc = coord_end[1] - coord_start[1]
+    keyr = iOPS[ (dr//abs(dr),0) ] * abs(dr) if dr!=0 else ''
+    keyc = iOPS[ (0,dc//abs(dc)) ] * abs(dc) if dc!=0 else ''
+    # in principle the keypresses can be either keyr+keyc or keyc+keyr
+    # however, one of these orders may not be allowed if a blank is crossed
+    keyrc = keyr+keyc+'A'
+    keycr = keyc+keyr+'A'
+    if coord_start[0]==coord_blank[0] and coord_end[1]==coord_blank[1]:
+        return [ keyrc ]
+    elif coord_start[1]==coord_blank[1] and coord_end[0]==coord_blank[0]:
+        return [ keycr ]
+    elif keyrc != keycr:
+        return [ keyrc, keycr ]
+    else:
+        return [ keyrc ]
+
+print('We are on: A>')
+print(get_key_pair(KDIR, 'A', '>'))
+print(get_key_pair(KDIR, '>', 'A'))
+print('We are on: A^')
+print(get_key_pair(KDIR, 'A', '^'))
+print(get_key_pair(KDIR, '^', 'A'))
+print('We are on: A<')
+print(get_key_pair(KDIR, 'A', '<'))
+print(get_key_pair(KDIR, '<', 'A'))
+print('We are on: Av')
+print(get_key_pair(KDIR, 'A', 'v'))
+print(get_key_pair(KDIR, 'v', 'A'))
+
+sys.exit('We are done')
+
+def get_key_sequence(pad, sequence):
+    for i in range(len(sequence)):
+        if i == 0:
+            Keys = get_key_pair(pad, 'A', sequence[i])
+        else:
+            Keys_New = get_key_pair(pad, sequence[i-1], sequence[i])
+            Keys = [prefix+suffix for suffix in Keys_New for prefix in Keys]
     return Keys
-                   
+
+def extract_min_len(items):
+    min_len = min([len(item) for item in items])
+    stripped = []
+    for item in items:
+        if len(item) == min_len:
+            stripped.append(item)
+    return stripped
+
 # parsing
 X = [l.strip() for l in open(sys.argv[1], 'r')]
 
 # part 1
+ans1 = 0
+ndir = 3
 for x in X:
-    # find the shortest paths for the numerical robot
-    Keys_Num = get_paths((3,2),KNUM,x)
-    # find the shortest paths for the first directional robot
-    Keys_Dir1 = get_paths((0,2),KDIR,Keys_Num[0])
-    # find the shortest paths for the second directional robot
-    Keys_Dir2 = get_paths((0,2),KDIR,Keys_Dir1[0])
-    #Keys_Dir2 = []
-    #for key in Keys_Dir1:
-    #    Keys_Dir2 += get_paths((0,2),KDIR,key)
-print(len('v<<A>>^A<A>AvA<^AA>A<vAAA>^A'))
-print(len(Keys_Dir1[0]))
-print(len('<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A'))
-print(len(Keys_Dir2[0]))
-
+    # get the sequence to be inputted on the first directional keypad
+    Keys = extract_min_len(get_key_sequence(KNUM, x))
+    print('We are on the numerical robot')
+    print('The number of keys is: ',len(Keys))
+    for i in range(ndir):
+        print('We are on directional robot: ',i)
+        Keys_Next = []
+        for buttons in Keys:
+            Keys_Next += get_key_sequence(KDIR, buttons)
+        Keys = extract_min_len(Keys_Next)
+        print('The number of keys is: ',len(Keys))
+        print(Keys[0:10])
+    shortest = min([len(buttons) for buttons in Keys])
+    ans1 += shortest * int(x[:-1])
+print(ans1)
 
