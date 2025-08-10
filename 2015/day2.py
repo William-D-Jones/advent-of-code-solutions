fc895b3d06bc506b73ebdd486e9b0d0a799b687a
William Jones <joneswilliamd@hotmail.com>
2025-08-10 15:56:33 -0400

    completed 2015 day 2

---
+++ 2015/day2.py
@@ -0,0 +1,27 @@
+import sys
+
+X = [ l.strip() for l in open(sys.argv[1], 'r') ]
+
+# parsing
+Dim = []
+for x in X:
+    l, w, h = map(int, x.split('x'))
+    Dim.append( (l,w,h) )
+
+# part 1
+ans1 = 0
+for l, w, h in Dim:
+    sa = 2*l*w + 2*w*h + 2*h*l
+    sl = min(l*w, w*h, h*l)
+    ans1 += sa + sl
+print(ans1)
+
+# part 2
+ans2 = 0
+for l, w, h in Dim:
+    wrap = min(2*(l+w), 2*(w+h), 2*(h+l))
+    bow = l*w*h
+    ans2 += wrap + bow
+print(ans2)
+
+
